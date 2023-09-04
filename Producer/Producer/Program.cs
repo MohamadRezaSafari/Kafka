81f2428b26e5bbeb0c93d6feca3b5fbebd6c2b14
Mohamad Reza Safari <Mohamad Reza Safari@DESKTOP-J76TGMQ>
2023-09-04 13:58:06 +0330

    Initial Producer service

---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Producer;
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, collection) =>
+        {
+            collection.AddHostedService<KafkaProducerHostedService>();
+        });
+
+
+CreateHostBuilder(args).Build().Run();
+
+Console.WriteLine("Hello, World!");
